ec4e1eaf7a9c3cca9bf4be585d176370effc8834
AbdoulSalam407 <abdoulsalamd742@gmail.com>
2025-06-21 19:28:15 +0000

    Préparation pour Railway

--- backend/settings.py
+++ backend/settings.py
@@ -79,18 +79,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-from decouple import config
+# Supprime ou commente cette partie si elle existe :
+# from decouple import config
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default='5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # <- Ce fichier sera créé automatiquement
     }
 }
+
 
 ALLOWED_HOSTS = ['*']
 

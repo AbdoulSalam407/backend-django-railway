2fa166f4de305fbd9d0b92b67326b4620165d178
AbdoulSalam407 <abdoulsalamd742@gmail.com>
2025-06-21 18:07:42 +0000

    first commit

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+
+class ProductList(APIView):
+    def get(self, request):
+        products = Product.objects.all().values()
+        return Response(products)
